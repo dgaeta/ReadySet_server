7143704142d929562f75dfeecd78162fa4f6482d
dhawking <gaeta.d@gmail.com>
2016-01-20 21:00:03 -0600

    cleaning up

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/var/www/parachute_server/venv"
+set -gx VIRTUAL_ENV "/Users/danielgaeta/Documents/Code/parachute_server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
